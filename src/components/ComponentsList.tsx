631dc752031a069012c0921bed87535a5aa2692a
Eugen Soloviov <suenot@gmail.com>
2024-04-08 18:02:25 +0300

    вынес роутинг в отдельный файл

---
+++ src/components/ComponentsList.tsx
@@ -0,0 +1,46 @@
+import { PaymentUi } from "./PaymentUi"
+import { Unit } from "./Unit"
+import { UnitList } from "./UnitList"
+import { WalletUi } from "./WalletUi"
+import { SchemaSandbox } from "./SchemaSandbox"
+import { WalletList } from "./WalletList"
+import { PortfolioList } from "./PortfolioList"
+import { LanguageList } from "./LanguageList"
+import { Echarts } from "./Echarts"
+import { Switch } from "../SwitchMode"
+import Dashboard from "./Dashboard"
+import { Candles } from "./Candles/TradingView/Candles"
+
+import {
+  ChakraProvider,
+  extendTheme,
+  Container
+} from "@chakra-ui/react"
+
+export const ComponentsList = () => {
+  return <Container>
+    <br />
+    <PaymentUi />
+    <br />
+    <Unit />
+    <br />
+    <UnitList />
+    <br />
+    <WalletList />
+    <br />
+    <PortfolioList />
+    <br />
+    <LanguageList />
+    <br />
+    <div>Old:</div>
+    <WalletUi />
+    <br />
+    <SchemaSandbox />
+    <br />
+    <Echarts />
+    <br />
+    <Dashboard />
+    <br />
+    <Switch />
+  </Container>
+}
